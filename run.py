d66b10382d8148c753f19e67af71cbae7e7a69f7
Kang, Ning <heath.kang@foxmail.com>
2023-04-02 21:47:16 +0800

    feat: add asyncio support.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
-from pyparsing import empty
-import requests
+import aiohttp
+import asyncio
 import rich
 from rich.prompt import Prompt
 from rich.console import Console
@@ -16,63 +16,96 @@ FAILED_LIST = []
 console = Console()
 
 
-def get_url(doc_id, doc_name, jwt_token):
+async def get_url(doc_id, doc_name, jwt_token):
     headers = {
         "jwt-token": jwt_token,
     }
     payload = {"docId": doc_id, "password": ""}
 
-    res = requests.post(url=GET_SINGLE_DOC_PATH, headers=headers, json=payload)
-    console.log(f"Fetching [bold blue]{doc_name}[/bold blue] content")
-    return res.json()["data"]["definition"]
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=GET_SINGLE_DOC_PATH, json=payload, headers=headers
+        ) as res:
+            try:
+                result = await res.json()
+                console.log(f"Fetching [bold blue]{doc_name}[/bold blue] content")
+                return (result["data"]["definition"], doc_name)
+            except Exception as e:
+                console.print_exception(e)
+                return None
 
 
-def write_to_pdf(data, doc_name, jwt_token):
+async def write_to_pdf(data, doc_name, jwt_token):
+    if data == None:
+        console.log(f"Get {data} for {doc_name} error.")
+        return
+
     headers = {"jwt-token": jwt_token, "origin": "https://mubu.com"}
     payload = {"type": "pdf", "definition": data}
-    res = requests.post(url=EXPORT_SINGLE_DOC_PATH, headers=headers, json=payload)
-
     file_name = PDF_PATH + doc_name + ".pdf"
     file_path = file_name.replace(file_name.split("/")[-1], "")
-    try:
-        if not os.path.exists(file_path):
-            os.makedirs(file_path)
-        with open(file_name, "wb") as f:
-            console.log(f"Writing content to [bold blue]{file_name}[/bold blue]")
-            f.write(res.content)
-    except Exception as e:
-        console.log(e)
-        FAILED_LIST.append(file_name)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=EXPORT_SINGLE_DOC_PATH, json=payload, headers=headers
+        ) as res:
+            try:
+                result = await res.content.read()
+
+                if not os.path.exists(file_path):
+                    os.makedirs(file_path)
+                with open(file_name, "wb") as f:
+                    console.log(
+                        f"Writing content to [bold blue]{file_name}[/bold blue]"
+                    )
+                    f.write(result)
+            except Exception as e:
+                console.log(e)
+                FAILED_LIST.append(file_name)
 
 
-def write_to_word(data, doc_name, jwt_token):
+async def write_to_word(data, doc_name, jwt_token):
+    if data == None:
+        console.log(f"Get {data} for {doc_name} error.")
+        return
+
     headers = {"jwt-token": jwt_token, "origin": "https://mubu.com"}
     payload = {"type": "docx", "definition": data}
-    res = requests.post(url=EXPORT_SINGLE_DOC_PATH, headers=headers, json=payload)
-
     file_name = DOCX_PATH + doc_name + ".docx"
     file_path = file_name.replace(file_name.split("/")[-1], "")
 
-    try:
-        if not os.path.exists(file_path):
-            os.makedirs(file_path)
-        with open(file_name, "wb") as f:
-            console.log(f"Writing content to [bold blue]{file_name}[/bold blue]")
-            f.write(res.content)
-    except Exception as e:
-        console.log(e)
-        FAILED_LIST.append(file_name)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=EXPORT_SINGLE_DOC_PATH, json=payload, headers=headers
+        ) as res:
+            try:
+                result = await res.content.read()
+
+                if not os.path.exists(file_path):
+                    os.makedirs(file_path)
+                with open(file_name, "wb") as f:
+                    console.log(
+                        f"Writing content to [bold blue]{file_name}[/bold blue]"
+                    )
+                    f.write(result)
+            except Exception as e:
+                console.log(e)
+                FAILED_LIST.append(file_name)
 
 
-def get_all_file_id_names(jwt_token: str) -> dict:
+async def get_all_file_id_names(jwt_token: str) -> dict:
     payload = {"start": ""}
     headers = {
         "jwt-token": jwt_token,
     }
-    res = requests.post(url=GET_ALL_DOCUMENTS_PATH, headers=headers, json=payload)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=GET_ALL_DOCUMENTS_PATH, json=payload, headers=headers
+        ) as res:
+            result = await res.json()
 
-    file_id_names = __generate_filenames(res.json())
-    return file_id_names
+            file_id_names = __generate_filenames(result)
+            return file_id_names
 
 
 def __generate_filenames(data: list) -> dict:
@@ -82,15 +115,16 @@ def __generate_filenames(data: list) -> dict:
     return file_id_names
 
 
-def get_jwt_token(phone: str, password: str) -> str:
+async def get_jwt_token(phone: str, password: str) -> str:
     payload = {"phone": phone, "password": password, "callbackType": 0}
 
-    res = requests.post(url=PHONE_LOGIN_PATH, json=payload)
-
-    if res.json()["code"] != 0:
-        console.log(f"[bold red]Error! please see the details \n {res.json()}")
-        raise Exception(res.json())
-    return res.json()["data"]["token"]
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=PHONE_LOGIN_PATH, json=payload) as res:
+            result = await res.json()
+            if result["code"] != 0:
+                console.log(f"[bold red]Error! please see the details \n {result}")
+                raise Exception(res)
+            return result["data"]["token"]
 
 
 def parse_command():
@@ -112,20 +146,25 @@ def parse_command():
     return username, password, file_type.get(file_type_number, "pdf")
 
 
-def main():
+async def main():
     rich.print(
         "[bold green]Welcome to use mubu-exporter,please follow the below guide to start!"
     )
     phone, password, file_type = parse_command()
     export_to_file = write_to_pdf if file_type == "pdf" else write_to_word
-    token = get_jwt_token(phone, password)
-    file_id_names = get_all_file_id_names(token)
+    token = await get_jwt_token(phone, password)
+    file_id_names = await get_all_file_id_names(token)
     with console.status("[bold green] Fetching data...") as status:
-        for doc_id, doc_name in file_id_names.items():
-            data = get_url(doc_id, doc_name, token)
-            export_to_file(data, doc_name, token)
+        url_doc_name_tasks = [
+            get_url(doc_id, doc_name, token)
+            for doc_id, doc_name in file_id_names.items()
+        ]
+        results = await asyncio.gather(*url_doc_name_tasks)
 
-    if FAILED_LIST is empty:
+        tasks = [export_to_file(url, doc_name, token) for url, doc_name in results]
+        _ = await asyncio.gather(*tasks)
+
+    if len(FAILED_LIST) == 0:
         rich.print(
             f"[blue]Export done, please check [bold]{PDF_PATH}[/bold] folder to see all documents."
         )
@@ -136,4 +175,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
